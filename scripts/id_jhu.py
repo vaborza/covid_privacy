56d4c8866bb79f28f4798225f9ba39a55c0b06ad
Victor Borza <vaborza@gmail.com>
2020-08-04 11:57:40 -0500

    Added choropleth map functionality and preliminary results

--- scripts/id_jhu.py
+++ scripts/id_jhu.py
@@ -8,6 +8,8 @@ import matplotlib.pyplot as plt
 from scipy.special import comb
 import seaborn as sns
 import random
+import plotly.figure_factory as ff
+import plotly.express as px
 
 # Apply the Golle formula, log must be used to prevent overflows
 def estimate_anon(pop,bins,k_level):
@@ -29,9 +31,10 @@ def gen_risk_ratio(sex_bins,race_bins,ethnicity_bins,age_specificity,age_cap,k_l
     # k_level: Set level of k-anonymity desired 
 
     df = pd.read_csv('../csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv')
-    df.drop(['UID','iso2','iso3','code3','FIPS','Admin2','Province_State','Country_Region','Lat','Long_'],
-            axis=1,inplace=True)
-    df.set_index('Combined_Key',inplace=True)
+    df.drop(['UID','iso2','iso3','code3','Combined_Key','Admin2','Province_State','Country_Region',
+        'Lat','Long_'], axis=1,inplace=True)
+
+    df.set_index('FIPS',inplace=True)
 
     # Calculate the rolling average using a 7-day window
     df_rolled = df.rolling(7,axis=1,center=False).mean()
@@ -69,7 +72,10 @@ def gen_heatmap(seed=7312020,sex_bins=2,race_bins=7,ethnicity_bins=2,
 def threshold_re_id(sex_bins=2,race_bins=7,ethnicity_bins=2,age_specificity=1,
         age_cap=90,k_level=1,id_cutoff=0.05):
 
-    #
+    # For a given sharing profile, estimate the number of counties that can safely share that data
+    # using a desired k level and population percentage
+    # id_cutoff: The percentage of the population that is acceptable to be identifiable at the given
+    #            k level or less
 
     df_risk_ratio = gen_risk_ratio(sex_bins,race_bins,ethnicity_bins,age_specificity,age_cap,k_level)
     safe_locales = (df_risk_ratio < id_cutoff).to_numpy().sum(axis=0) / len(df_risk_ratio.index)
@@ -85,4 +91,30 @@ def threshold_re_id(sex_bins=2,race_bins=7,ethnicity_bins=2,age_specificity=1,
             '\n# Ethnicities: ' + str(ethnicity_bins) + '\nAge Specificity: ' +
             str(age_specificity) + '\nAge Cap: ' + str(age_cap),xy=(0.2,0.7),xycoords='figure fraction')
     plt.show()
+
+###
+
+def gen_geomap(sex_bins=2,race_bins=7,ethnicity_bins=2,age_specificity=1,age_cap=90,k_level=1,
+        date='4/1/20'):
+
+    #
     
+    df_risk_ratio = gen_risk_ratio(sex_bins,race_bins,ethnicity_bins,age_specificity,age_cap,k_level)
+    df_risk_ratio = df_risk_ratio[df_risk_ratio.index.notna() & df_risk_ratio[date].notna()]
+    fips = (df_risk_ratio.index.values)
+    values = df_risk_ratio[date]
+
+    cmap = px.colors.sequential.Plasma
+    endpts = [0.01, 0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95]
+
+    fig = ff.create_choropleth(fips=fips,values=values,colorscale=cmap,round_legend_values=False,
+            title=('COVID Data Sharing Risk on ' + date + ' at k-level ' + str(k_level) +
+            '<br />Using: ' + str(sex_bins) + ' sexes, ' + str(race_bins) + ' races, ' + 
+            str(ethnicity_bins) + ' ethnicities, Age in ' + str(age_specificity) + 
+            '-year bins, until ' + str(age_cap) + ' years'), 
+            binning_endpoints = endpts, legend_title='Re-ID risk')
+    fig.layout.template = None
+    fig.show()
+
+
+
